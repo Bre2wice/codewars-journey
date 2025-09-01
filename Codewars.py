bb922f94be5778816a306da70c1c5178d8e39d58
Brianna Franklin <soul4musiq11@gmail.com>
2025-09-01 12:42:57 -0400

    Initial commit

---
+++ Codewars.py
@@ -0,0 +1,15 @@
+# 1. Even or Odd solution
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# 2. Convert a Number to a String solution
+def number_to_string(num):
+    return str(num)
+
+# 3. Remove String Spaces solution
+def no_space(x):
+    return x.replace(" ", "")
